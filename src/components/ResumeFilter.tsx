25be77e78452faa59620958a0c250017af5f497a
Vickus <vanzylv@gmail.com>
2022-06-04 08:04:49 +1200

    Resume filter

--- src/components/ResumeFilter.tsx
+++ src/components/ResumeFilter.tsx
@@ -1,19 +1,53 @@
-import { SegmentedControl, SegmentedControlItem } from '@mantine/core'
+import { Center, Container, SegmentedControl, SegmentedControlItem, Space } from '@mantine/core'
 import { useState } from 'react'
 import { DevTags } from '../interfaces/DevTagsInterface'
 
 const devTags: DevTags = require('../data/tags.json')
 
 const ResumeFilter = (): JSX.Element => {
-    const [value, setValue] = useState('all')
+    const [category, setCategory] = useState('all')
+    const [subCategory, setSubCategory] = useState('all')
 
-    let data: SegmentedControlItem[] = [{ value: 'all', label: 'All' }]
+    let categoryData: SegmentedControlItem[] = [{ value: 'all', label: 'All' }]
+    let subCategoryData: SegmentedControlItem[] = []
 
     devTags.tags.forEach(({ category, description }) => {
-        return data.push({ label: description, value: category })
+        return categoryData.push({ label: description, value: category })
     })
 
-    return <SegmentedControl fullWidth value={value} onChange={setValue} data={data} />
+    const subCategories = devTags.tags.filter((item) => item.category === category)
+
+    if (subCategories.length > 0) {
+        subCategoryData.push({ value: 'all', label: 'All' })
+        subCategories[0].frameworks.forEach(({ name, description }) => {
+            return subCategoryData.push({ label: description, value: name })
+        })
+    }
+
+    return (
+        <>
+            <Container fluid style={{ margin: 10 }}>
+                <SegmentedControl
+                    fullWidth
+                    value={category}
+                    onChange={setCategory}
+                    data={categoryData}
+                />
+            </Container>
+            <Space />
+            <Container fluid style={{ margin: 10, minHeight: 25 }}>
+                {subCategoryData.length > 1 ? (
+                    <Center>
+                        <SegmentedControl
+                            value={subCategory}
+                            onChange={setSubCategory}
+                            data={subCategoryData}
+                        />
+                    </Center>
+                ) : null}
+            </Container>
+        </>
+    )
 }
 
 export default ResumeFilter
