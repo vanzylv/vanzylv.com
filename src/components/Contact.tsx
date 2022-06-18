2dfbbd7fc8ffc144b67a1131b0d2b505d14e0cf2
Vickus <vanzylv@gmail.com>
2022-06-19 11:22:44 +1200

    align contact

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -1,27 +1,23 @@
-import { Container, Stack } from '@mantine/core'
+import { Stack } from '@mantine/core'
 import { BrandGithub, BrandLinkedin, Mail } from 'tabler-icons-react'
 
-const About = () => (
-    <Stack>
-        <Container fluid>
-            <Stack>
-                <div>
-                    <BrandGithub />
-                    <a href="https://github.com/vanzylv/">Github</a>
-                </div>
+const Contact = () => (
+    <Stack align="start">
+        <div>
+            <BrandGithub />
+            <a href="https://github.com/vanzylv/">Github</a>
+        </div>
 
-                <div>
-                    <BrandLinkedin />
-                    <a href="https://www.linkedin.com/in/vickus-van-zyl-65932a163/">Linkedin</a>
-                </div>
+        <div>
+            <BrandLinkedin />
+            <a href="https://www.linkedin.com/in/vickus-van-zyl-65932a163/">Linkedin</a>
+        </div>
 
-                <div>
-                    <Mail />
-                    <a href="mailto:vanzylv@gmail.com">vanzylv@gmail.com</a>
-                </div>
-            </Stack>
-        </Container>
+        <div>
+            <Mail />
+            <a href="mailto:vanzylv@gmail.com">vanzylv@gmail.com</a>
+        </div>
     </Stack>
 )
 
-export default About
+export default Contact
